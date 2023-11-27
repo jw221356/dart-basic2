3c7e9d4cbee1f75f6828098653b220943b253924
jeong won <jw221356@naver.com>
2023-11-27 17:25:05 +0900

    first commit

---
+++ bin/untitled.dart
@@ -0,0 +1,5 @@
+import 'package:untitled/11_27/main.dart' as untitled;
+
+void main(List<String> arguments) {
+  print('Hello world: ${untitled.calculate()}!');
+}
---
+++ test/untitled_test.dart
@@ -0,0 +1,8 @@
+import 'package:untitled/11_27/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
