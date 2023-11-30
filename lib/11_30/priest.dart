33c5a0f8d5a3cdd582e63fc631ec5356eded6f6e
jeong won <jw221356@naver.com>
2023-11-30 18:00:38 +0900

    연습문제 1-3

--- lib/11_30/priest.dart
+++ lib/11_30/priest.dart
@@ -1,7 +1,21 @@
+
+void main() {
+  Cleric cleric = Cleric();
+  cleric.selfAid();
+}
+
 class Cleric {
   String name = 'priest';
   int hp = 50;
   final int maxhp = 50;
   int mp = 10;
   final int maxmp = 10;
+
+  void selfAid() {
+    if (mp > 5 ){
+      mp -=5;
+      hp = maxhp;
+    }
+  }
+
 }
