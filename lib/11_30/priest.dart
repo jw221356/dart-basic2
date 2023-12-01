63df3f38728677ac82164314db757e9afc7b1613
jeong won <jw221356@naver.com>
2023-12-01 11:24:49 +0900

    연습문제 1-4

--- lib/11_30/priest.dart
+++ lib/11_30/priest.dart
@@ -1,21 +1,42 @@
+import 'dart:math';
 
 void main() {
   Cleric cleric = Cleric();
   cleric.selfAid();
+
+  int sec = 4;
+  int realRecoverymp = cleric.pray(sec);
+  print('$realRecoverymp 회복함');
 }
 
 class Cleric {
+  final int maxmp = 10;
+  final int maxhp = 50;
   String name = 'priest';
   int hp = 50;
-  final int maxhp = 50;
   int mp = 10;
-  final int maxmp = 10;
 
   void selfAid() {
-    if (mp > 5 ){
-      mp -=5;
+    if (mp >= 5) {
+      mp -= 5;
       hp = maxhp;
     }
   }
 
+  int pray(int sec) {
+    Random random = Random();
+
+    final int beforemp = mp;
+
+    final int recoverymp = sec + Random().nextInt(3);
+
+    final int realRecoverymp = mp - beforemp;
+    mp += recoverymp;
+
+    if (mp > maxmp) {
+      mp = maxmp;
+    }
+
+    return beforemp - mp;
+  }
 }
