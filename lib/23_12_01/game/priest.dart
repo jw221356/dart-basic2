50af53fa6833892e16ffc824b3d33b333ffdd343
jeong won <jw221356@naver.com>
2023-12-01 15:33:30 +0900

    연습문제 2-2

--- lib/23_12_01/game/priest.dart
+++ lib/23_12_01/game/priest.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
-
 void main() {
-  Cleric cleric = Cleric();
+  Cleric cleric = Cleric(name:'priest');
   cleric.selfAid();
 
   int sec = 4;
@@ -10,11 +9,22 @@ void main() {
 }
 
 class Cleric {
-  final int maxmp = 10;
-  final int maxhp = 50;
-  String name = 'priest';
-  int hp = 50;
-  int mp = 10;
+ static const int maxmp = 50;
+ static const int maxhp = 10;
+  String name;
+  int hp;
+  int mp;
+
+ Cleric({
+   required this.name,
+   this.hp = maxhp,
+   this.mp = maxmp,
+ });
+
+  final cleric1 = Cleric(name:'아서스', hp:40, mp:5);
+  final cleric2 = Cleric(name:'아서스', hp:35);
+  final cleric3 = Cleric(name:'아서스');
+
 
   void selfAid() {
     if (mp >= 5) {
