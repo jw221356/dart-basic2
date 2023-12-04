5d888f56c79cac554b521022b48e7b3090ef94b0
jeong won <jw221356@naver.com>
2023-12-04 17:18:22 +0900

    연습문제 2-1

---
+++ lib/23_12_04/collection_2-1.dart
@@ -0,0 +1,14 @@
+void main() {
+  // 대한민국의 도시 이름 모음 (순서 상관 없음)
+  Set<String> koreaCity = {
+
+  };
+  //10명 학생의 시험 점수
+  Map<String, int> studentScore = {
+
+  };
+  //대한민국의 도시별 인구수 (순서 상관 없음)
+  Map<String, int> KoreaPopulation = {
+
+  };
+}
