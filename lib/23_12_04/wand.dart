644aa7cd3767cf6b8a346458cc9acc4bd3628d3f
jeong won <jw221356@naver.com>
2023-12-05 09:46:08 +0900

    getter, setter 추가

--- lib/23_12_04/wand.dart
+++ lib/23_12_04/wand.dart
@@ -8,14 +8,36 @@ class Wand {
     required name,
     required power,
   })  : _name = name,
-        _power = power;
+        _power = power{
+        _validName(_name);
+        _validPower(_power);
+  }
 
-  set name(String name){
-    if (_name.length < 3) {
-      throw Exception('지팡이 이름은 3글자 이상이어야 한다.');
+  String get name => _name;
+
+  set name(String name) {
+    _validName(name);
+    _name = name;
+  }
+
+  double get power => _power;
+
+  set power(double power) {
+    _validPower(power);
+    _power = power;
+  }
+  void _validName(String name) {
+    if (name.length < 3) {
+      throw Exception('지팡이 이름은 3글자 이상이여야 한다.');
     }
-    if (_power < 0.5 || _power > 100.0) {
-      throw Exception('지팡이 마력은 0.5 이상 100.0 이하여야 한다.');
+  }
+
+  void _validPower(double power) {
+    if (power < 0.5) {
+      throw Exception('지팡이 마력은 0.5 이상이어야 한다.');
+    }
+    if (power > 100.0) {
+      throw Exception('지팡이 마력은 100 이하여야 한다.');
     }
   }
 }
--- lib/23_12_04/wizard.dart
+++ lib/23_12_04/wizard.dart
@@ -14,17 +14,54 @@ class Wizard {
   })  : _name = name,
         _hp = hp,
         _mp = mp,
-        _wand = wand;
+        _wand = wand{
+        _validName(name);
+        _validHp(hp);
+        _validMp(mp);
+  }
 
-  set name(String name){
-    if (_name.length < 3) {
+  String get name => _name;
+
+  set name(String name) {
+    _validName(name);
+    _name = name;
+  }
+
+  int get hp => _hp;
+
+  set hp(int hp) {
+    _validHp(hp);
+    _hp = hp;
+  }
+
+  int get mp => _mp;
+
+  set mp(int mp) {
+    _validMp(mp);
+    _mp = mp;
+  }
+
+  Wand get wand => _wand;
+
+  set wand(Wand wand) {
+    _wand = wand;
+  }
+
+  void _validName(String name) {
+    if (name.length < 3) {
       throw Exception('마법사 이름은 3글자 이상이여야 한다.');
     }
-    if (_hp < 0) {
-      throw Exception('HP가 음수가 되는 상황에서는 대신 0을 설정 되도록 한다.');
+  }
+
+  void _validHp(int hp) {
+    if (hp < 0) {
+      _hp = 0;
     }
-    if (_mp < 0) {
-      throw Exception('마법사의 MP는 0 이상이어야 한다. ');
+  }
+
+  void _validMp(int mp) {
+    if (mp < 0) {
+      throw Exception('마법사의 MP는 0 이상이어야 한다.');
     }
   }
 }
