a314bb05d84df01777ebed026e5adc1e73bac959
jeong won <jw221356@naver.com>
2023-12-11 10:30:55 +0900

    12-08 과제

--- lib/23_12_08/string.dart
+++ lib/23_12_08/string.dart
@@ -1,21 +1,17 @@
 //i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오
 // 영어에서의 모음은 a, e, i, o, u 다섯가지이다
 
-class Word{
+class Word {
   String word = '';
 
-  bool isVowel(int i){
-    return word.substring(i, i + 1) == 'a';
+  bool isVowel(int i) {
+    String vowel = word.substring(i, i + 1);
+    return ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].contains(vowel);
   }
 
-//i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오
-  bool isConsonant(int i){
-    return false;
+  bool isConsonant(int i) {
+    return !isVowel(i);
   }
-}
-
-void main() {
-
 }
 
 
