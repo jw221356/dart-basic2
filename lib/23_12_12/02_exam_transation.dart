bdaf8132e9a67c7fc018ea7b70a92a33cd1bb169
jeong won <jw221356@naver.com>
2023-12-13 12:01:32 +0900

    12_12 과제

--- lib/23_12_12/02_exam_transation.dart
+++ lib/23_12_12/02_exam_transation.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 
 class Trader {
@@ -5,6 +7,11 @@ class Trader {
   String city;
 
   Trader(this.name, this.city);
+
+  @override
+  String toString() {
+    return '이름: $name, 도시: $city';
+  }
 }
 
 class Transaction {
@@ -13,7 +20,13 @@ class Transaction {
   int value;
 
   Transaction(this.trader, this.year, this.value);
+
+  @override
+  String toString() {
+    return '트랜잭션 트레이더: $trader 년도: $year 값: $value\n';
+  }
 }
+
 
 var transactions = [
   Transaction(Trader("Brian", "Cambridge"), 2011, 300),
@@ -26,45 +39,46 @@ var transactions = [
 
 void main() {
   // 1. 2011년에 일어난 모든 트랜잭션을 찾아 가격 기준 오름차순으로 정리하여 이름을 나열하시오
-  print(transactions.where((e) => e.year == 2011)
+  print(transactions
+      .where((e) => e.year == 2011)
       .sorted((a, b) => a.value.compareTo(b.value))
       .map((e) => e.trader.name)
       .toSet()
       .toList()
-      .length);
+  );
+  // 2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
+  print(transactions.map((e) => e.trader.city).toSet().toList());
 
-  final result = transactions
-      .where((e) => e.year == 2011)
-      .toList();
-  // print(transactions.where((e) => e.year == 2011).toList().length);
+  // 3. 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
+  print(transactions
+      .where((e) => e.trader.city == "Cambridge")
+      .map((e) => e.trader.name)
+      .toSet()
+      .toList());
 
-  // print(transactions.map((e) => e.trader.name).toSet().toList());
-
-  // 2. map 거래자가 근무하는 모든 도시를 중복 없이 나열하시오
-  print(transactions.map((e) => e.trader.city).toSet());
-
-  // 3. where sort 케임브리지에서 근무하는 모든 거래자를 찾아서 이름순으로 정렬하여 나열하시오
-  final a = transactions
-      .where((e) => e.trader.city.contains("Cambridge"))
-      .map((e) => e.trader.city)
-      .toList();
-
-  final b = transactions
-      .where((e) => e.trader.city == "Cambridge") // where 조건 필터링 찾기
-      .map((e) => e.trader.name) //map 추출
-      .toList();
-  a.sort((a, b) => a.compareTo(b));
-
-  print(b);
-
-  // 4. sort 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
-  final v = transactions
+  // 4. 모든 거래자의 이름을 알파벳순으로 정렬하여 나열하시오
+  print(transactions
       .map((e) => e.trader.name)
       .toList()
-      .sort((a, b) => a.compareTo(b));
+      .toSet()
+      .sorted((a, b) => compareNatural(a, b)));
 
-  print(transactions.map((e) => e.trader.name).toSet().toList()
-    ..sort((a, b) => a.compareTo(b)));
-  }
+// 5. 밀라노에 거래자가 있는가? 아니면 any를 사용하여 밀라노에 거래자가 있는지만 출력하는 것인가?
+  print(transactions.any((e) => e.trader.city == "Milan"));
 
-
+// 6. 케임브리지에 거주하는 거래자의 모든 트랜잭션값을 출력하시오
+  print(transactions
+      .where((e) => e.trader.city == "Cambridge")
+      .toList()
+  );
+// 7. 전체 트랜잭션 중 최대값은 얼마인가?
+  print(transactions
+      .map((e) => e.value)
+      .reduce((v, e) => max(v, e))
+  );
+// 8. 전체 트랜잭션 중 최소값은 얼마인가?
+  print(transactions
+      .map((e) => e.value)
+      .reduce((v, e) => min(v, e))
+  );
+}
