c33c747b149131441f0b839c89768595e39ce7b9
jeong won <jw221356@naver.com>
2023-12-15 15:15:37 +0900

    12_14 과제 (진행중)

--- lib/23_12_14/test/http_test3.dart
+++ lib/23_12_14/test/http_test3.dart
@@ -4,7 +4,7 @@ import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final bytes = await downloadImage('https://alimipro.com/favicon.ico');
+  final bytes = await downloadImage('https://www.naver.com/asdfasdf');
   // print(bytes);
 
   await saveFile(bytes, 'icon.ico');
@@ -13,8 +13,13 @@ void main() async {
 Future<Uint8List> downloadImage(String url) async {
   // return http.readBytes(Uri.parse(url));
   final response = await http.get(Uri.parse(url));
-  print(response.statusCode);
-  return response.bodyBytes;
+  if (response.statusCode == HttpStatus.ok) {
+    print(response.statusCode);
+    return response.bodyBytes;
+  } else {
+    throw http.ClientException('잘못된 url', Uri.parse(url));
+  }
+
 }
 
 Future<File> saveFile(Uint8List bytes, String fileName) async {
